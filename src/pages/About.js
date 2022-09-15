93174fccc3fc931a4d1722d95ab22318a8c3c3b6
Diego Batista Pereira <diegobpcelular@gmail.com>
2022-09-15 02:36:29 -0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sunt
+        possimus, laboriosam consequuntur sapiente aspernatur suscipit nam
+        nostrum, dolorem a accusamus, maiores illo! Aspernatur quod assumenda
+        perspiciatis asperiores reprehenderit tenetur dolores, eligendi neque
+        vel, quaerat voluptas fuga error facilis modi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
